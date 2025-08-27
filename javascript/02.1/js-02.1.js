e6e9de648a09eb0779e58aec8633eac524d78715
Arthur A <fatec-dsm2@academico.intra>
2025-08-27 20:51:40 -0300

    Objetos e classes

--- javascript/02.1/js-02.1.js
+++ javascript/02.1/js-02.1.js
@@ -1,15 +1,17 @@
 //Arrays (vetores): Servem para armazenar uma lista, ou seja, diferentes elementos em única variável
-let Produtos = ['Computador','Notebook','Celular','Tablet'];
+let Produtos = ["Computador", "Notebook", "Celular", "Tablet"];
 document.write(`<p>${Produtos}</p>`);
-document.write(`<p>Exibindo um elemento do array através do índice: ${Produtos[3]}</p>`);
-document.write(typeof(Produtos));
+document.write(
+  `<p>Exibindo um elemento do array através do índice: ${Produtos[3]}</p>`
+);
+document.write(typeof Produtos);
 
 document.write("<p>Percorrendo um array através do forEach:</p>");
-Produtos.forEach(produto=>{
-    document.write(`<p>${produto}</p>`)
-})
+Produtos.forEach((produto) => {
+  document.write(`<p>${produto}</p>`);
+});
 
 document.write("<p>Mostrando os índices e valores com forEach:</p>");
-Produtos.forEach((produto,i)=>{
-    document.write(`<p>${i+1} - ${produto}</p>`)
-})
+Produtos.forEach((produto, i) => {
+  document.write(`<p>${i + 1} - ${produto}</p>`);
+});
